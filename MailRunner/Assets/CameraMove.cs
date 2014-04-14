6cbb65f98aaeca8d49a754a1ec8072e5254b2538
Sarah Strawn <sarah.c.strawn@gmail.com>
2014-04-14 16:25:57 -0700

    Made bladder meter and energy meter

--- MailRunner/Assets/CameraMove.cs
+++ MailRunner/Assets/CameraMove.cs
@@ -7,14 +7,25 @@ public class CameraMove : MonoBehaviour {
 	void Start () {
 	
 	}
+
+	public float speed = 0.1F;
+	public GameObject emeter;  
+
+
 	
 	// Update is called once per frame
 	void Update () {
+		emeter = GameObject.Find ("EnergyMeter");
+		if (emeter.transform.localScale.y <= 0.001) {
+						speed = 0.1F;
+		} else {
+			speed = emeter.transform.localScale.y * 3;
+		}
 		if (Input.GetKey(KeyCode.W)) {
-			transform.localPosition += new Vector3(0, 0, 0.1F);
+			transform.localPosition += new Vector3(0, 0, 1F * speed);
 		}
 		if (Input.GetKey (KeyCode.S)) {
-			transform.localPosition += new Vector3(0, 0, -0.1F);
+			transform.localPosition += new Vector3(0, 0, 1F * speed );
 		}
 	}
 }
